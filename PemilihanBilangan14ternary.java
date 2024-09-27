3e39ce6c3e60c7faf3df9996b6163e1534ee978b
muchnabil <muchammadnabil@gmail.com>
2024-09-27 12:54:59 +0700

    Jobsheet 4

---
+++ PemilihanBilangan14ternary.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PemilihanBilangan14ternary {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan sebuah angka : ");
+        int angka = sc.nextInt();
+        String hasil;
+
+        hasil = (angka % 2 == 0) ? "Bilangan genap" : "Bilangan ganjil";
+        System.out.println(angka + " adalah " + hasil);
+    }
+}
