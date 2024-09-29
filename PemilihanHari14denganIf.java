0aad93eeda9fb4f9b194958f79c08f219f2897a9
muchnabil <muchammadnabil@gmail.com>
2024-09-29 07:40:55 +0700

    Jobsheet 4

---
+++ PemilihanHari14denganIf.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class PemilihanHari14denganIf {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan angka : ");
+        int angka = sc.nextInt();
+
+        if (angka >= 1 && angka <= 5){
+            System.out.print(angka + " is weekday");
+        }
+        else if (angka == 6 || angka == 7){
+            System.out.print(angka + "is weekend");
+        }
+        else {
+            System.out.print(angka + " invalid number");
+        }
+    }
+}
